3350f6b581c0e7abb54d44b8d4b201cd1266d308
Aditya73singh <99402057+Aditya73singh@users.noreply.github.com>
2025-03-08 16:19:37 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,10 @@
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  score: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
